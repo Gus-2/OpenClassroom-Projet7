bb65f181bf4073c1d8bd360034f3b3bf1a66d78b
de Meeûs Augustin <demeeus.augustin@outlook.com>
2020-01-17 18:19:10 +0100

    Add the gradle dependencies And the FirebaseUi to connect the user

--- app/src/main/java/com/openclassroom/go4lunch/MainActivity.java
+++ app/src/main/java/com/openclassroom/go4lunch/MainActivity.java
@@ -1,14 +1,75 @@
 package com.openclassroom.go4lunch;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
+
+    private ConstraintLayout constraintLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        constraintLayout = findViewById(R.id.main_activity_layout);
+        startSignInActivity();
+    }
+
+    private void startSignInActivity(){
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(
+                                Arrays.asList( //EMAIL
+                                        new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
+                                        new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build())) // SUPPORT GOOGLE
+                        .setIsSmartLockEnabled(false, true)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        this.handleResponseAfterSignIn(requestCode, resultCode, data);
+    }
+
+    private void showSnackBar(ConstraintLayout relativeLayout, String message){
+        Snackbar.make(relativeLayout, message, Snackbar.LENGTH_SHORT).show();
+    }
+
+    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data){
+
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        if(requestCode == RC_SIGN_IN){
+            if(resultCode == RESULT_OK){ // SUCCESS
+                showSnackBar(this.constraintLayout, getString(R.string.connection_secceded));
+            }else{ // ERRORS
+                if(response == null){
+                    showSnackBar(this.constraintLayout, getString(R.string.error_authentification_canceled));
+                }else if(response.getErrorCode() == ErrorCodes.NO_NETWORK){
+                    showSnackBar(this.constraintLayout, getString(R.string.error_no_internet));
+                }else if(response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR){
+                    showSnackBar(this.constraintLayout, getString(R.string.error_unknow_error));
+                }
+            }
+        }
     }
 }
